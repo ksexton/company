98df920ac269cda5b0f307c9cccc1f52a31f1fc6
Kyle Sexton <ks@mocker.org>
2015-04-30 13:23:47 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to company
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/company/core_test.clj
@@ -0,0 +1,7 @@
+(ns company.core-test
+  (:require [clojure.test :refer :all]
+            [company.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
